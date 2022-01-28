fa76940a29edff98d648b7c5779765d001e84e83
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-01-28 15:16:00 -0500

    UserTrait:validate

--- app/Http/Controllers/Traits/UserTrait.php
+++ app/Http/Controllers/Traits/UserTrait.php
@@ -10,9 +10,9 @@ trait UserTrait
 {
     use RestActions;
 
-    public function saveUser($request)
+    public function valide($request)
     {
-        $validator = Validator::make(
+        return Validator::make(
             $request->all(),
             [
                 'parent_id' => 'nullable|exists:users,id',
@@ -27,9 +27,14 @@ trait UserTrait
                 'state' => 'required|numeric',
             ]
         );
+    }
+
+    public function saveUser($request)
+    {
+        $validator = $this->valide($request);
 
         if ($validator->fails()) {
-            return $this->respond(500,  $validator->errors(), 'validation error' . $validator->errors()->first());
+            return $this->respond(500,  $validator->errors(), 'validation error', $validator->errors()->first());
         }
 
         try {
@@ -48,7 +53,39 @@ trait UserTrait
 
             return $this->respond(200, $user, null, 'Usuario creado exitosamente');
         } catch (\Exception $e) {
-            return $this->respond(500, [], $e->getMessage() . 'Error al crear usuario');
+            return $this->respond(500, [], $e->getMessage(), 'Error al crear usuario');
+        }
+    }
+
+    public function updateUser($request)
+    {
+        $validator = $this->valide($request);
+
+        if ($validator->fails()) {
+            return $this->respond(500,  $validator->errors(), 'validation error', $validator->errors()->first());
+        }
+
+        try {
+            $user = User::find($request->id);
+            if (is_null($user)) {
+                return $this->respond(500, [], 'user not found', 'No se encontro el usuario');
+            }
+            $user->update([
+                'parent_id' => $request->parent_id,
+                'name' => $request->name,
+                'last_name' => $request->last_name,
+                'document_type' => $request->document_type,
+                'document_number' => $request->document_number,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'password' => $request->password,
+                'role' => $request->role,
+                'state' => $request->state
+            ]);
+
+            return $this->respond(200, $user, null, 'Usuario actualizado exitosamente');
+        } catch (\Exception $e) {
+            return $this->respond(500, [], $e->getMessage(), 'Error al actualizar usuario');
         }
     }
 }
