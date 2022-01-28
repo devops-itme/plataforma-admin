aa89c359c76730497db7e4a786f0ab8444843cc7
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-01-28 11:20:05 -0500

    parameter values seeder: added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         $this->call(ParameterValueSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
-=======
-        $this->call([
-            RoleSeeder::class,
-            UserSeeder::class
-        ]);
->>>>>>> 7110de5377e454e03f6686f4f11fb51a8c171e68
     }
 }
