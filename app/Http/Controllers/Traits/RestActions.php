0cf83f17773b1bc6690e9db7244c77100fbd4674
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-01-28 11:43:20 -0500

    REST_Actions:respond

---
+++ app/Http/Controllers/Traits/RestActions.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Traits;
+
+trait RestActions
+{
+    public function respond($state, $data = [], $error = null, $message = '')
+    {
+        return [
+            'state' => $state, //response status
+            'data' => $data, //response data
+            'error' => $error, //bug for developer
+            'message' => $message //user message
+        ];
+    }
+}
