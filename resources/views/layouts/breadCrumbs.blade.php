6544bd3e78ecdb93a87ec88e8fe51f6ac13d7622
Fredys Muriel <72772732+FredysMuriel4@users.noreply.github.com>
2022-02-07 15:47:14 -0500

    users breadcrumbs done

--- resources/views/layouts/breadCrumbs.blade.php
+++ resources/views/layouts/breadCrumbs.blade.php
@@ -3,7 +3,7 @@
         <li class="breadcrumb-item Active"><a href="/" style="color: #6f6f6f !important;">Inicio</a></li>
         @foreach (explode(".",request()->route()->getName()) as $item)
             @if ($item!="index")
-                <li class="breadcrumb-item active" {aria-current="page" }> <a href="{{request()->segment(1)==__("$item")?'/'.request()->segment(1):'#'}}" style="color: #6f6f6f !important;">{{__("$item")}}</a> </li>
+                <li class="breadcrumb-item active" {aria-current="page" }> <a href="{{ucfirst(request()->segment(1))==__("$item")?'/'.request()->segment(1):'#'}}" style="color: #6f6f6f !important;">{{__("$item")}}</a> </li>
             @endif
         @endforeach
     </ol>
