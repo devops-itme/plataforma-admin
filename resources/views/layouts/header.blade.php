3e949498eb153cbe34593b8f1ed16e40f1293fa1
Omyy27 <omovisolutions@gmail.com>
2022-02-18 13:32:54 -0500

    Fix text Logout

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -25,7 +25,9 @@
                 <a class="dropdown-item" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
+                    <div>
+                        <p class="mb-0">Cerrar Sesión</p>
+                    </div>
                 </a>
 
              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
