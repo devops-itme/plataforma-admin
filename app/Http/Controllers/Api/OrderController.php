0acf5260a721fcd5de0b97b260b85068d88857bb
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-02-23 11:16:45 -0500

    MessengerSeeder

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Traits\RestActions;
 use App\Http\Resources\OrderResource;
 use App\Order;
+use App\ParameterValue;
 use App\Route;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
@@ -18,13 +19,20 @@ class OrderController extends Controller
     public function index(Request $request)
     {
         $messenger_user_id = $request->messenger_user_id ?? Auth::user()->id;
-
+        $relationships = [
+            'getUser', 'getUser.getDocumentType',
+            'getOrderType', 'getDocumentType', 'getPaymentMethod',
+            'getState', 'getDepartment', 'getBranchOffice'
+        ];
         try {
+            $state = ParameterValue::where('name', 'Despachados')->first();
             $orders = Order::whereHas('getGuides', function (Builder $query) use ($messenger_user_id) {
                 $query->whereHas('getRoute', function (Builder $query) use ($messenger_user_id) {
                     $query->where('messenger_user_id', $messenger_user_id);
                 });
-            })->with(['getUser', 'getUser.getDocumentType', 'getOrderType', 'getDocumentType', 'getPaymentMethod', 'getState', 'getDepartment', 'getBranchOffice'])->get();
+            })
+                ->where('state', $state->id)
+                ->with($relationships)->get();
             $orders = OrderResource::collection($orders);
             return $this->respond(200, $orders, null, 'Ordenes asignadas');
         } catch (\Throwable $e) {
--- public/js/app.js
+++ public/js/app.js
@@ -7836,7 +7836,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n.active_row {\n    background: #2f45b5;\n    color: #ffff;\n}\n", ""]);
+exports.push([module.i, "\n.active_row {\r\n    background: #2f45b5;\r\n    color: #ffff;\n}\r\n", ""]);
 
 // exports
 
@@ -62215,13 +62215,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
-__webpack_require__(/*! /home/germanvq/jobProjects/developapp/Admin-Multientrega-v2/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/germanvq/jobProjects/developapp/Admin-Multientrega-v2/resources/sass/app.scss */"./resources/sass/app.scss");
-=======
 __webpack_require__(/*! D:\Laravel\Admin-Multientrega-v2\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! D:\Laravel\Admin-Multientrega-v2\resources\sass\app.scss */"./resources/sass/app.scss");
->>>>>>> df11eda23e40dd8ea347b69a90c91b49c683297d
 
 
 /***/ })
