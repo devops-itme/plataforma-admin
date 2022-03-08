b25157f4065dd4502157da131dc15f196ece2cae
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-03-08 09:30:56 -0500

    app_status added in order&guide

--- database/migrations/2022_01_27_162824_create_guides_table.php
+++ database/migrations/2022_01_27_162824_create_guides_table.php
@@ -40,7 +40,7 @@ class CreateGuidesTable extends Migration
             $table->integer('packaging')->nullable();
             $table->unsignedBigInteger('customer_address')->nullable();
             $table->integer('state')->default(32)->comment("{1:Por despachar;2:Despachado;3:Completado}");
-            $table->integer('app_status')->nullable();
+            $table->integer('app_status')->nullable()->default(0)->comment("{0:Pendiente;1:Leído;}");
             $table->timestamps();
             $table->softDeletes();
         });
