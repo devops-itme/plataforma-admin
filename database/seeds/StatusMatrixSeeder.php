21ab59617712354cd11b20fcd7ce913bd34b691e
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-03-09 11:56:47 -0500

    StatusMatrixSeeder

---
+++ database/seeds/StatusMatrixSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\ParameterValue;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Config;
+
+class StatusMatrixSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $system_status = Config::get('const.system_status');
+        // foreach ($system_status as $key => $values) {
+        //     $scope = ParameterValue::where('name',$key)->first();
+        //     foreach ($values as $value) {
+               
+        //     }
+        // }
+    }
+}
