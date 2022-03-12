38e6d4cd7e2424650e45f12aebefbf6ac572f590
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-03-12 12:33:45 -0500

    API: create order init

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -83,7 +83,7 @@ class OrderController extends Controller
             }
             $guides = $request->guides;
             foreach($guides as $guide){
-                $guide = json_decode($guide);
+                $guide = json_encode($guide);
                 return ($guide);
             }
             
