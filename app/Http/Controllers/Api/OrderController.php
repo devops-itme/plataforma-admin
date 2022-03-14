448eca00de97ea40581ba59e6af146f863c49c70
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-03-14 13:14:58 -0500

    API: create order init

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -90,17 +90,13 @@ class OrderController extends Controller
             foreach ($guides as $guide) {
                 $array = new Collection([
                     'guide_description' => $guide['guide_description'],
-                    'address_name' => $guide['address_name'],
-                    'address_lat' => $guide['address_lat'],
-                    'address_lng' => $guide['address_lng'],
-                    'address_description' => $guide['address_description'],
                     'contact' => $guide['contact'],
                     'phone_contact' => $guide['phone_contact'],
                     'email_contact' => $guide['email_contact'],
                     'return_last_destination' => $guide['return_last_destination'],
                 ]);
 
-                $address = Address::find($guide->address_id);
+                $address = Address::find($guide['address_id']);
                 if (!is_null($address)) {
                     $array->merge([
                         'address_name' => $address->name,
