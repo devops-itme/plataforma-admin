8fd4164a23e4742313b3868cb8789a364ef237e2
Fredys Muriel <72772732+FredysMuriel4@users.noreply.github.com>
2022-03-24 09:47:30 -0500

    Add validate only to non deletes orders

--- database/migrations/2022_01_27_162016_create_orders_table.php
+++ database/migrations/2022_01_27_162016_create_orders_table.php
@@ -15,7 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('order_number')->unique();
+            $table->string('order_number');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->integer('order_type');
