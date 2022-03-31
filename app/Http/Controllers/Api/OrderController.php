d9477912bfa306dc009e74385f39e26f86001414
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-03-31 16:43:25 -0500

    storeOrder

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -142,7 +142,7 @@ class OrderController extends Controller
                     $validator = $this->AddressesValidate($request);
 
                     if ($validator->fails()) {
-                        return $this->respond(500, [],  $validator->errors(),  $validator->errors()->first());
+                        return $this->respond(500, [],  $validator->errors() . ' - address',  $validator->errors()->first());
                     }
 
                     if ($guide['add_address_favorite'] === 'true') {
