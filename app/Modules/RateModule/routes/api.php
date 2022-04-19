70e80005d1ea53c308048cf59f8a4c336d4f2cae
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-04-19 10:10:45 -0500

    routes/api:rates

--- app/Modules/RateModule/routes/api.php
+++ app/Modules/RateModule/routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::resource('rates', 'RateModule\Controllers\Api\RateController')->names('rates');
+    Route::resource('rates', 'RateModule\Controllers\Api\RateController')->names('rates-api');
 });
