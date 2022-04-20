536b9d225d175d382f9c72e08fac9a7e2dece365
Fredys Muriel <72772732+FredysMuriel4@users.noreply.github.com>
2022-04-20 13:19:26 -0500

    privatized save evidence route

--- app/Modules/GuideModule/routes/api.php
+++ app/Modules/GuideModule/routes/api.php
@@ -7,5 +7,5 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('guide/markAsRead', 'GuideModule\Controllers\Api\GuideController@markAsRead');
     Route::resource('guides', 'GuideModule\Controllers\Api\GuideController')->names('guides');
     Route::put('guide/changeStatus', 'GuideModule\Controllers\Api\GuideController@changeStatus')->name('guide.changeStatus');
+    Route::post('guide/saveEvidence', 'GuideModule\Controllers\Api\GuideController@saveEvidence');
 });
-Route::post('guide/saveEvidence', 'GuideModule\Controllers\Api\GuideController@saveEvidence');
