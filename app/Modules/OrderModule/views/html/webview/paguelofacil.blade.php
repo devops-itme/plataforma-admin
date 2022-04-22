b18ea2756aa29285b241a17789c50ebbfa35aa31
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-04-22 16:41:22 -0500

    sendNotification done

--- app/Modules/OrderModule/views/html/webview/paguelofacil.blade.php
+++ app/Modules/OrderModule/views/html/webview/paguelofacil.blade.php
@@ -5,24 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>Multientrega</title>
     {{-- Favicon --}}
     <link rel="shortcut icon" href="{{ asset('/img/favicon-2.png') }}" />
 
-    <!-- Scripts -->
-    <script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9MFKCZ2zM_6wtlJCiaSdalzbubH_tKFk&libraries=places"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/tooltip.js') }}"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script src="{{ asset('js/alert.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <script src="{{ asset('js/apexcharts.js') }}"></script>
-    <script src="{{ asset('js/selectBranchOffice.js') }}"></script>
-
+  
 
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
