d55c6b38c670c8fd256a3f50408b864e7b0c3c75
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-04-28 14:57:28 -0500

    test notification

--- public/js/app.js
+++ public/js/app.js
@@ -2280,8 +2280,9 @@ var Orders = /*#__PURE__*/function () {
                 state = state.value;
                 notification_type = notification_type.value;
                 fcm_token = fcm_token.value;
+                alert(fcm_token);
                 url = "".concat(window.location.origin, "/api/sendPushNotification?state=").concat(state, "&notification_type=").concat(notification_type, "&fcm_token=").concat(fcm_token);
-                _context10.next = 11;
+                _context10.next = 12;
                 return fetch(url).then(function (response) {
                   return response.json();
                 }).then(function (data) {
@@ -2291,7 +2292,7 @@ var Orders = /*#__PURE__*/function () {
                   console.log(e);
                 });
 
-              case 11:
+              case 12:
               case "end":
                 return _context10.stop();
             }
