184477103d4fe0a5b5fb258a1c6636e6d8e0fc37
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-04-28 15:16:59 -0500

    test notification

--- app/Modules/OrderModule/routes/api.php
+++ app/Modules/OrderModule/routes/api.php
@@ -8,6 +8,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::put('order/finishOrder', 'OrderModule\Controllers\Api\OrderController@finishOrder')->name('order.finishOrder');
     Route::put('order/changeStatus', 'OrderModule\Controllers\Api\OrderController@changeStatus')->name('order.changeStatus');
 
+    Route::get('order/webview/paguelo-facil', 'OrderModule\Controllers\Api\OrderController@webviewPagueloFacil')->name('order.webview');
+    Route::get('order/webview/paguelo-facil/response', 'OrderModule\Controllers\Api\OrderController@responseViewPagueloFacil')->name('order.webview.response');
 });
-Route::get('order/webview/paguelo-facil', 'OrderModule\Controllers\Api\OrderController@webviewPagueloFacil')->name('order.webview');
-Route::get('order/webview/paguelo-facil/response', 'OrderModule\Controllers\Api\OrderController@responseViewPagueloFacil')->name('order.webview.response');
