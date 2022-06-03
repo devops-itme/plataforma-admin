d10318dcc68f257891e1e467086180647326a765
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-06-03 13:38:21 -0500

    test order api

--- app/Modules/OrderModule/Controllers/Api/OrderController.php
+++ app/Modules/OrderModule/Controllers/Api/OrderController.php
@@ -234,7 +234,7 @@ class OrderController extends Controller
                             'type' => 'package_picture',
                             'document' => $picture,
                         ]);
-                        // return $this->respond(500, $picture, 'not found', 'test de imagen');
+                        return $this->respond(500, $picture, 'not found', 'test de imagen');
                         $storeDocumentResponse = $GuidanceDocumentController->store($request);
                         if ($storeDocumentResponse['state'] != 200) {
                             return $storeDocumentResponse;
