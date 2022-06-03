89a77c377da744ef3f40bac019fb71b9a4b0c283
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-06-03 11:50:21 -0500

    test order api

--- app/Modules/OrderModule/Controllers/Api/OrderController.php
+++ app/Modules/OrderModule/Controllers/Api/OrderController.php
@@ -99,6 +99,7 @@ class OrderController extends Controller
 
     public function store(Request $request)
     {
+        return $this->respond(500, $request->all(), 'not found', 'test');
         try {
             if (Auth()->user()->role != 1) {
                 $request->merge(['user_id' => Auth()->user()->id]);
