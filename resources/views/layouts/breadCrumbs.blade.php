eae608bfe958e66063c859616fb21f8718e492a4
Omyy27 <omovisolutions@gmail.com>
2022-06-04 09:51:02 -0500

    Fix breadCrumbs

--- resources/views/layouts/breadCrumbs.blade.php
+++ resources/views/layouts/breadCrumbs.blade.php
@@ -4,16 +4,16 @@
        @foreach (explode(".",request()->route()->getName()) as $item)
           @if ($item!="index")
              @if($item == "internationalOrders")
-                <li class="breadcrumb-item active" {aria-current="page"}>  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):route('internationalOrders.index')}}" style="color: #6f6f6f !important;">{{__("Ordenes Internacionales")}}</a> </li>
+                <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):route('internationalOrders.index')}}" style="color: #6f6f6f !important;">{{__("Ordenes Internacionales")}}</a> </li>
                 @elseif ($item!="internationalOrders")
                 @if ($item == "shipments")
-                <li class="breadcrumb-item active" {aria-current="page"}>  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):route('internationalOrders.index')}}" style="color: #6f6f6f !important;">{{__("Ordenes Internacionales")}}</a> </li>
-                <li class="breadcrumb-item active" {aria-current="page"}>  <a href="javascript:history.back()" style="color: #6f6f6f !important;">{{__("Guias")}}</a> </li>
+                <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):route('internationalOrders.index')}}" style="color: #6f6f6f !important;">{{__("Ordenes Internacionales")}}</a> </li>
+                <li class="breadcrumb-item active" aria-current="page">  <a href="javascript:history.back()" style="color: #6f6f6f !important;">{{__("Guias")}}</a> </li>
                 @elseif ($item!="shipments")
-                <li class="breadcrumb-item active" {aria-current="page"}>  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):'#'}}" style="color: #6f6f6f !important;">{{__("$item")}}</a> </li>
+                <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):'#'}}" style="color: #6f6f6f !important;">{{__("$item")}}</a> </li>
                 @endif
              @else
-             <li class="breadcrumb-item active" {aria-current="page"}>  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):'#'}}" style="color: #6f6f6f !important;">{{__("$item")}}</a> </li>
+             <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):'#'}}" style="color: #6f6f6f !important;">{{__("$item")}}</a> </li>
             @endif
           @endif
        @endforeach
