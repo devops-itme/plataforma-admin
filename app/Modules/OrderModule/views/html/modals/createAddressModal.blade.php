4f6d4d09c235888a2f17a74acce1cda1e555a39a
Kevin Causado <66138769+Gladiatore32@users.noreply.github.com>
2022-06-06 12:54:28 -0500

    MUL -696, Correcion consola

--- app/Modules/OrderModule/views/html/modals/createAddressModal.blade.php
+++ app/Modules/OrderModule/views/html/modals/createAddressModal.blade.php
@@ -8,7 +8,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <div hidden id="modal_body"></div>
+                <div hidden id="modal_data_user_id"></div> <!-- Capture user_id from view Orders(Create) -->
                 <form action="{{ route('addresses.store') }}" method="POST" id="formCreateAddress" name="myform">
                     @csrf
                     {{-- <input type="text"  id="user_code" hidden name="user_id" value="{{$customer->user_id}}"> --}}
@@ -34,14 +34,3 @@
         </div>
     </div>
 </div>
-
-
-<script type="text/javascript">
-    function save() {
-        var $popup = $('#modal_body').text();
-        // window.alert($popup); 
-
-        var myvalue = $popup;
-        $("#user_code").val(myvalue);
-    }
-</script>
--- app/Modules/OrderModule/views/html/national/create.blade.php
+++ app/Modules/OrderModule/views/html/national/create.blade.php
@@ -223,19 +223,19 @@
 </div>
 @endsection
 
-<!-- Import jquery cdn -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-</script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
-</script>
-
 <script type="text/javascript">
     function submit() {
         // window.alert("MODAL ABIERTO");
         var user_id = $("#user_id").val();
-        var marks = $("#marks").val();
         var str = user_id;           
-         $("#modal_body").html(str);
+         $("#modal_data_user_id").html(str);
+    }
+
+    function save() {
+        var $popup = $('#modal_data_user_id').text();
+        // window.alert($popup); 
+
+        var myvalue = $popup;
+        $("#user_code").val(myvalue);
     }
 </script>
