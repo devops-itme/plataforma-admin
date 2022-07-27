1e81e4cf588879e6c82157b0712f5321bb234387
Kevin Causado <66138769+Gladiatore32@users.noreply.github.com>
2022-07-26 19:11:15 -0500

    MUL-874

--- public/js/app.js
+++ public/js/app.js
@@ -9801,7 +9801,9 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
       if (this.searchMessenger) {
         return this.messengers.filter(function (item) {
           return _this2.searchMessenger.toString().toLowerCase().split(" ").every(function (v) {
-            return item.user.document_number.toLowerCase().includes(v) || item.user.name.toLowerCase().includes(v);
+            var _item$user$document_n, _item$user$document_n2, _item$user$name, _item$user$name$toLow;
+
+            return ((_item$user$document_n = item.user.document_number) === null || _item$user$document_n === void 0 ? void 0 : (_item$user$document_n2 = _item$user$document_n.toLowerCase()) === null || _item$user$document_n2 === void 0 ? void 0 : _item$user$document_n2.includes(v)) || ((_item$user$name = item.user.name) === null || _item$user$name === void 0 ? void 0 : (_item$user$name$toLow = _item$user$name.toLowerCase()) === null || _item$user$name$toLow === void 0 ? void 0 : _item$user$name$toLow.includes(v));
           });
         });
       }
@@ -15403,7 +15405,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n.active_row {\n    background: #2f45b5;\n    color: #ffff;\n}\n.urgent_row {\n    background: #d31928;\n    color: #ffff;\n}\n", ""]);
+exports.push([module.i, "\n.active_row {\r\n    background: #2f45b5;\r\n    color: #ffff;\n}\n.urgent_row {\r\n    background: #d31928;\r\n    color: #ffff;\n}\r\n", ""]);
 
 // exports
 
@@ -15441,7 +15443,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n.sortableSelected {\n    background-color: #023E8A;\n    color: #fff;\n}\n", ""]);
+exports.push([module.i, "\n.sortableSelected {\r\n    background-color: #023E8A;\r\n    color: #fff;\n}\r\n", ""]);
 
 // exports
 
@@ -90861,8 +90863,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Desktop\DevelopApp\Projects\MultientregaProject\Admin-Multientrega-v2\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Desktop\DevelopApp\Projects\MultientregaProject\Admin-Multientrega-v2\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\Admin-Multientrega-v2\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Admin-Multientrega-v2\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
