13bd3bc7b13323a395e1784436e25b7f0fdc74c1
Kevin Causado <66138769+Gladiatore32@users.noreply.github.com>
2022-08-01 13:53:58 -0500

    MUL-925,Detail User

--- app/Modules/UserModule/views/html/detail.blade.php
+++ app/Modules/UserModule/views/html/detail.blade.php
@@ -13,63 +13,50 @@
     </div>
     @include('layouts.alerts')
 
-    <div class="card-body d-flex flex-row flex-wrap">
-
-        <div class="form-group col-md-3">
-            <label>Nombres</label>
-            <input type="text" class="form-control form-control-solid" placeholder="Nombres" name="name" value="{{$user->name}}" disabled />
-            <span class="form-text text-muted"></span>
-        </div>
-        <div class="form-group col-md-3">
-            <label>Apellidos</label>
-            <input type="text" class="form-control form-control-solid" placeholder="Apellidos" name="last_name" value="{{$user->last_name}}" disabled />
-        </div>
-        <div class="form-group col-md-3">
-            <label>Email</label>
-            <input type="email" class="form-control form-control-solid" placeholder="Email" name="email" value="{{$user->email}}" disabled />
-            <span class="form-text text-muted"></span>
-        </div>
-        <div class="form-group col-md-3">
-            <label>Estado</label>
-            <input type="text" class="form-control form-control-solid" name="state" value="{{$user->state==1 ? 'Activo':'Inactivo'}}" disabled />
-            <span class="form-text text-muted"></span>
-        </div>
-        <div class="form-group col-md-3">
-            <label>Teléfono</label>
-            <input type="tel" class="form-control form-control-solid" placeholder="Teléfono" name="phone" value="{{$user->phone}}" disabled />
-            <span class="form-text text-muted"></span>
-        </div>
-        <div class="form-group col-md-3">
-            <label>Rol</label>
-            <select class="form-control form-control-solid" id="document_type" name="role" disabled>
-                <option>{{$user->getRole->name}}</option>
-                <option value="Admin">Admin</option>
-                <option value="Operador">Operador</option>
-                <option value="Mensajero">Mensajero</option>
-            </select>
-            <span class="form-text text-muted"></span>
-        </div>
-        <div class="form-group col-md-3">
-            <label>Tipo de documento</label>
-            <select class="form-control form-control-solid" id="document_type" name="role" disabled>
-                <option>{{$user->getDocumentType->name}}</option>
-                <option value="CC">C.C</option>
-                <option value="CE">C.E</option>
-                <option value="PE">P.E</option>
-                <option value="RUC">RUC</option>
-            </select>
-            <span class="form-text text-muted"></span>
+    <div class="card-body ">
+        <div class="border-bottom mb-5 pb-5">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="font-weight-bolder mb-3">Nombres:</div>
+                    <div class="line-height-xl">{{ $user->name ?? 'No registra'}}</div>
+                </div>
+                <div class="col-md-3">
+                    <div class="font-weight-bolder mb-3">Apellidos:</div>
+                    <div class="line-height-xl">{{$user->last_name ?? 'No registra'}}</div>
+                </div>
+                <div class="col-md-3">
+                    <div class="font-weight-bolder mb-3">Email:</div>
+                    <div class="line-height-xl">{{$user->email ?? 'No registra'}}</div>
+                </div>
+                <div class="col-md-3">
+                    <div class="font-weight-bolder mb-3">Estado:</div>
+                    <div class="line-height-xl">{{$user->state==1 ? 'Activo':'Inactivo' ??'No registra'}}</div>
+                </div>
+            </div>
         </div>
 
-        <div class="form-group col-md-3">
-            <label>Número de identificación</label>
-            <input type="text" class="form-control form-control-solid" placeholder="N° de identificación" name="document_number" value="{{$user->document_number}}" disabled />
-            <span class="form-text text-muted"></span>
+        <div class="border-bottom mb-5 pb-5">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="font-weight-bolder mb-3">Teléfono:</div>
+                    <div class="line-height-xl">{{$user->phone ?? 'No registra'}}</div>
+                </div>
+                <div class="col-md-3">
+                    <div class="font-weight-bolder mb-3">Rol:</div>
+                    <div class="line-height-xl">{{$user->getRole->name ?? 'No registra'}}</div>
+                </div>
+                <div class="col-md-3">
+                    <div class="font-weight-bolder mb-3">Tipo de documento:</div>
+                    <div class="line-height-xl">{{$user->getDocumentType->name ?? 'No registra'}}</div>
+                </div>
+                <div class="col-md-3">
+                    <div class="font-weight-bolder mb-3">Número de identificación:</div>
+                    <div class="line-height-xl">{{$user->document_number ?? 'No registra'}}</div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-@endsection
+        @endsection
 
-{{-- Styles Section --}}
-@section('styles')
-@endsection
+        {{-- Styles Section --}}
+        @section('styles')
+        @endsection
