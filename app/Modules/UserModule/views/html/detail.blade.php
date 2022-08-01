918672c28a7252dc14c664684891b936f668f529
Kevin Causado <66138769+Gladiatore32@users.noreply.github.com>
2022-08-01 12:30:40 -0500

    MUL-925

--- app/Modules/UserModule/views/html/detail.blade.php
+++ app/Modules/UserModule/views/html/detail.blade.php
@@ -3,51 +3,71 @@
 
 {{-- Content --}}
 @section('content')
-    @include('layouts.breadCrumbs')
+@include('layouts.breadCrumbs')
 
-    <div class="card card-custom">
-        <div class="card-header">
-            <h3 class="card-title">
-                Detalle del usuario
-            </h3>
-        </div>
-        @include('layouts.alerts')
-
-        <form action="" method="post">
-
-            <div class="card-body d-flex flex-row flex-wrap">
-                <div class="col-md-3">
-                    <div class="font-weight-bolder">Nombres:</div>
-                    <div class="line-height-xl">Admin</div>
-                </div>
-                <div class="col-md-3">
-                    <div class="font-weight-bolder">Numero de documento:</div>
-                    <div class="line-height-xl"></div>
-                </div>
-                <div class="col-md-3">
-                    <div class="font-weight-bolder">Correo:</div>
-                    <div class="line-height-xl">admin@me.com</div>
-                </div>
-                <div class="col-md-3">
-                    <div class="font-weight-bolder">Teléfono:</div>
-                    <div class="line-height-xl"></div>
-                </div>
-                <div class="col-md-6">
-                    <div class="font-weight-bolder mt-5">Rol:</div>
-                    <div class="line-height-xl">Admin</div>
-                </div>
-                <div class="col-md-6">
-                    <div class="font-weight-bolder mt-5">Estado:</div>
-                    <div class="line-height-xl">Activo</div>
-                </div>
-            </div>
-
-            <div class="card-footer d-flex justify-content-end">
-                <button type="reset" class="btn btn-secondary"><a href="{{ route('users.index') }}"
-                        class="text-muted">Volver</a></button>
-            </div>
-        </form>
+<div class="card card-custom">
+    <div class="card-header">
+        <h3 class="card-title">
+            Detalle del usuario
+        </h3>
     </div>
+    @include('layouts.alerts')
+
+    <div class="card-body d-flex flex-row flex-wrap">
+
+        <div class="form-group col-md-3">
+            <label>Nombres</label>
+            <input type="text" class="form-control form-control-solid" placeholder="Nombres" name="name" value="{{$user->name}}" disabled />
+            <span class="form-text text-muted"></span>
+        </div>
+        <div class="form-group col-md-3">
+            <label>Apellidos</label>
+            <input type="text" class="form-control form-control-solid" placeholder="Apellidos" name="last_name" value="{{$user->last_name}}" disabled />
+        </div>
+        <div class="form-group col-md-3">
+            <label>Email</label>
+            <input type="email" class="form-control form-control-solid" placeholder="Email" name="email" value="{{$user->email}}" disabled />
+            <span class="form-text text-muted"></span>
+        </div>
+        <div class="form-group col-md-3">
+            <label>Estado</label>
+            <input type="text" class="form-control form-control-solid" name="state" value="{{$user->state==1 ? 'Activo':'Inactivo'}}" disabled />
+            <span class="form-text text-muted"></span>
+        </div>
+        <div class="form-group col-md-3">
+            <label>Teléfono</label>
+            <input type="tel" class="form-control form-control-solid" placeholder="Teléfono" name="phone" value="{{$user->phone}}" disabled />
+            <span class="form-text text-muted"></span>
+        </div>
+        <div class="form-group col-md-3">
+            <label>Rol</label>
+            <select class="form-control form-control-solid" id="document_type" name="role" disabled>
+                <option>{{$user->getRole->name}}</option>
+                <option value="Admin">Admin</option>
+                <option value="Operador">Operador</option>
+                <option value="Mensajero">Mensajero</option>
+            </select>
+            <span class="form-text text-muted"></span>
+        </div>
+        <div class="form-group col-md-3">
+            <label>Tipo de documento</label>
+            <select class="form-control form-control-solid" id="document_type" name="role" disabled>
+                <option>{{$user->getDocumentType->name}}</option>
+                <option value="CC">C.C</option>
+                <option value="CE">C.E</option>
+                <option value="PE">P.E</option>
+                <option value="RUC">RUC</option>
+            </select>
+            <span class="form-text text-muted"></span>
+        </div>
+
+        <div class="form-group col-md-3">
+            <label>Número de identificación</label>
+            <input type="text" class="form-control form-control-solid" placeholder="N° de identificación" name="document_number" value="{{$user->document_number}}" disabled />
+            <span class="form-text text-muted"></span>
+        </div>
+    </div>
+</div>
 @endsection
 
 {{-- Styles Section --}}
