faf6f2db3ac9aa8b9066b87e392e56430622e6f6
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2022-08-04 14:12:42 -0500

    MUL-853

--- app/Modules/OrderModule/Controllers/Api/OrderController.php
+++ app/Modules/OrderModule/Controllers/Api/OrderController.php
@@ -124,7 +124,7 @@ class OrderController extends Controller
                 $source_rate = $Rate->calculateRate($rate->id);
 
                 $user_id = Auth::user()->id;
-                $request->merge(['user_id' => $user_id, 'address_description' => $request->address_description]);
+                $request->merge(['user_id' => $user_id, 'description' => $request->address_description]);
                 
                 if (!is_null($request->address_id)) {
                     $address = Address::find($request->address_id);
