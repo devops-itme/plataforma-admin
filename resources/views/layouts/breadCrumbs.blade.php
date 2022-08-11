904633568f5f9a1beed5777ab7a033a1d9b7edfc
Kevin Causado <66138769+Gladiatore32@users.noreply.github.com>
2022-08-11 16:26:21 -0500

    MUL-953

--- resources/views/layouts/breadCrumbs.blade.php
+++ resources/views/layouts/breadCrumbs.blade.php
@@ -9,12 +9,26 @@
                 @if ($item == "shipments")
                 <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):route('internationalOrders.index')}}" style="color: #6f6f6f !important;">{{__("Ordenes Internacionales")}}</a> </li>
                 <li class="breadcrumb-item active" aria-current="page">  <a href="javascript:history.back()" style="color: #6f6f6f !important;">{{__("Guias")}}</a> </li>
-                @elseif ($item!="shipments")
+                @elseif ($item!="shipments" && $item!="users" && $item!="customers" && $item!="messengers" && $item!="orders")
                 <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):'#'}}" style="color: #6f6f6f !important;">{{__("$item")}}</a> </li>
                 @endif
              @else
              <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):'#'}}" style="color: #6f6f6f !important;">{{__("$item")}}</a> </li>
             @endif
+
+           @if($item == "users")
+            <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):route('users.index')}}" style="color: #6f6f6f !important;">{{__("Usuarios")}}</a> </li>
+          @endif
+          @if($item == "customers")
+            <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):route('customers.index')}}" style="color: #6f6f6f !important;">{{__("Clientes")}}</a> </li>
+          @endif
+          @if($item == "messengers")
+            <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):route('messengers.index')}}" style="color: #6f6f6f !important;">{{__("Mensajeros")}}</a> </li>
+          @endif
+          @if($item == "orders")
+            <li class="breadcrumb-item active" aria-current="page">  <a href="{{request()->segment(1)==$item?'/'.request()->segment(1):route('orders.index')}}" style="color: #6f6f6f !important;">{{__("Ordenes")}}</a> </li>
+          @endif
+
           @endif
        @endforeach
     </ol>
