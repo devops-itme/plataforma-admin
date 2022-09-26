6e0abba52d2506ed673bd597077d1926b9c9a2de
Ozaky <adonisxavier06@gmail.com>
2022-09-26 15:25:28 -0500

    Deleting updated changes

--- app/Modules/GuideLogModule/Controllers/Api/GuideLogController.php
+++ app/Modules/GuideLogModule/Controllers/Api/GuideLogController.php
@@ -49,38 +49,4 @@ class GuideLogController extends Controller
             return $this->respond(500, null, $e->getMessage(), 'Error del servidor');
         }
     }
-
-    public function updateGuideLog(Request $request)
-    {
-        try {
-            // $guide = Guide::findOrFail($request->id);
-            // $guide->update([
-            //     'additional_address' => $request->additional_address,
-            //     'additional_email' => $request->additional_email,
-            //     'additional_phone' => $request->additional_phone,
-            //     'address_name' => $request->address,
-            //     'app_status' => $request->app_status,
-            //     'concept' => $request->concept,
-            //     'contact' => $request->contact,
-            //     'email_contact' => $request->contact_email,
-            //     'phone_contact' => $request->contact_phone,
-            //     'customer_document_type' => $request->document_type,
-            //     'transport_type' => $request->transport_type,
-            //     'value' => $request->value,
-            //     'value_corp' => $request->corp_value,
-            //     'novelty' => $request->novelty
-            // ]);
-
-            // $guide_id = $guide->order_id;
-            $guide_log = GuideLog::where('id','202')->findOrFail();
-
-            $guide_log->update([
-             'issue_id' => '58',
-         ]);
-
-            return $this->respond(200,$guide_log,'Log de Guía actualizada exitosamente');
-        } catch (\Exception $e) {
-            return $this->respond(500, [], $e->getMessage(), 'Error al actualizar el log de guía');
-        }
-    }
 }
