11056777fec3418590b5215df08580c911af147b
Kevin Causado <66138769+Gladiatore32@users.noreply.github.com>
2022-10-11 16:55:03 -0500

    MS-485, Protecting routes API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
+
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +52,16 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+
+        if ($request->expectsJson()) {
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    'status'    => 404,
+                    'message'   => 'Ruta no encontrada.',
+                ], 404);
+            }
+        }
+
         return parent::render($request, $exception);
     }
 }
