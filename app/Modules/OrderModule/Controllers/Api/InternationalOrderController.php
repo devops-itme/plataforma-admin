e585dc830b7a959d2bc076452c37c7e059a33893
Ozaky <adonisxavier06@gmail.com>
2022-11-10 11:48:46 -0500

    setting v3

--- app/Modules/OrderModule/Controllers/Api/InternationalOrderController.php
+++ app/Modules/OrderModule/Controllers/Api/InternationalOrderController.php
@@ -97,7 +97,7 @@ class InternationalOrderController extends Controller
 
     public function services(Request $request)
     {
-        //$user_id = Auth::user()->id;
+        $user_id = Auth::user()->id;
 
         $fecha_begin = date('Y-m-d 00:00:00', ($request->begin / 1000));
         $fecha_end = date('Y-m-d 23:59:59', ($request->end / 1000));
@@ -105,11 +105,11 @@ class InternationalOrderController extends Controller
         $query = DB::table('tealca_datas as t')
         ->select('t.id','t.external_id as external_id', 't.contact as contact', 't.date_status as FechaTime', 't.status as Status', 't.historical')
         //->whereRaw("DATEDIFF('" . Carbon::now() . "',g.created_at)  < 92 AND g.status_matrix_id != 10")
-       /*  ->join('orders as o', 'o.id', '=', 't.order_id')
+        ->join('orders as o', 'o.id', '=', 't.order_id')
         ->join('users as u', 'u.id', '=', 'o.user_id')
         ->join('guides as g', 'g.id', '=', 't.guide_id')
-        ->whereBetween(DB::raw('DATE(g.created_at)'), [$fecha_begin, $fecha_end]) */
-        //->where('u.id', $user_id)
+        ->whereBetween(DB::raw('DATE(g.created_at)'), [$fecha_begin, $fecha_end])
+        ->where('u.id', $user_id)
         ->get();
 
 
