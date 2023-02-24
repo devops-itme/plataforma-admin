cdb9e34522fa2ea788b3a8368372c6763c883b90
LangelU <62520537+LangelU@users.noreply.github.com>
2023-02-24 16:46:26 -0500

    Update DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,7 +24,6 @@ class DatabaseSeeder extends Seeder
             CorregimientoSeeder::class,
             NeighborhoodSeeder::class,
             CustomerSeeder::class,
-            CoordinadoraCitiesSeeder::class
         ]);
     }
 }
