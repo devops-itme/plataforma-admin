ed6a0d618ff3386e9dde06fac3e2ee362e5ba79c
LangelU <62520537+LangelU@users.noreply.github.com>
2023-04-05 09:32:22 -0500

    Fix DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
             CorregimientoSeeder::class,
             NeighborhoodSeeder::class,
             CustomerSeeder::class,
-            CoordinadoraCitiesSeeder::class,
+            //CoordinadoraCitiesSeeder::class,
         ]);
     }
 }
