e5f670220fcd696c7e9fe1eb0f7194a8cc1b2b2b
Ozaky <adonisxavier06@gmail.com>
2023-04-25 13:31:51 -0500

    setting validate route api send guide for coordinadora

--- app/Modules/ApiConnectionsModule/routes/api.php
+++ app/Modules/ApiConnectionsModule/routes/api.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::post('import-shipments', 'ApiConnectionsModule\Controllers\TealcaController@importShipments');
+Route::post('coordinadora/send/guides/{order_id}', 'ApiConnectionsModule\Controllers\CoordinadoraController@generateGuides');
