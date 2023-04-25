4d6ee1f9776001522f2daaeaebada1c55d0904c8
Ozaky <adonisxavier06@gmail.com>
2023-04-25 14:48:00 -0500

    setting validate route

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware(['auth:sanctum'])->group(function () {
 
 // });
-Route::post('coordinadora/send/guides/{order_id}', 'ApiConnectionsModule\Controllers\CoordinadoraController@generateGuides');
+Route::post('coordinadora/send/guides/{order_id}', 'App\Modules\ApiConnectionsModule\Controllers\CoordinadoraController@generateGuides');
