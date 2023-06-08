6b350570274ae3902c3240c55c0a92f0197ec185
Ozaky <adonisxavier06@gmail.com>
2023-06-08 16:24:18 -0500

    setting validate send guide

--- app/Modules/GuideModule/Controllers/ShipmentController.php
+++ app/Modules/GuideModule/Controllers/ShipmentController.php
@@ -374,7 +374,7 @@ class ShipmentController extends Controller
         $Tealca = new Tealca();
         $Tealca->login();
         $guideResponse = $Guide->getGuidesByOrder($id, false);
-        return $guideResponse;
+        return dd($guideResponse);
         if ($guideResponse['state'] != 200) {
             return $guideResponse;
         }
